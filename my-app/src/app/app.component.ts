ba114c9d4fff05acd404136569f7d656ae81ed3c
shailijha <shaili_jha@yahoo.co.in>
2021-07-19 11:07:50 -0500

    directives assignment

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -8,7 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
   username = '';
+  bool = true;
+  counter = 0;
+  clicks = [];
+
   resetUsername() {
     this.username = '';
+  }
+
+  btnClick() {
+    this.bool = !this.bool;
+    this.counter++;
+    this.clicks.push(this.counter);
+  }
+
+  applyBg(idx) {
+    console.log(idx);
+    return idx >= 4 ? 'blue' : 'white';
   }
 }
